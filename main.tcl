444d34e5b7a49c13801ce9067ed506120d883490
Jack Johnson <jack@localhost.localdomain>
2014-08-05 18:34:36 -0700

    ssl ne1?

--- main.tcl
+++ main.tcl
@@ -157,7 +157,7 @@ proc readreq {chan addr} {
 
 proc acceptconn {chan addr port} {
 	global waiting
-	fconfigure $chan -blocking 0 -buffering line -translation {binary binary}
+	fconfigure $chan -blocking 0 -buffering line -encoding binary -translation {binary binary}
 	set waiting($chan) 1
 	fileevent $chan readable [list readreq $chan $addr]
 }
@@ -166,7 +166,7 @@ proc sacceptconn {chan addr port} {
 	global waiting
 	fconfigure $chan -blocking 1 -buffering line
 	::tls::handshake $chan
-	fconfigure $chan -blocking 0 -buffering line -translation {binary binary}
+	fconfigure $chan -blocking 0 -buffering line -encoding binary -translation {binary binary}
 
 	set waiting($chan) 1
 	fileevent $chan readable [list readreq $chan $addr]
