9b5338c8466e44d223392439b9c5f589dc015e0d
Jack Johnson <jack@localhost.localdomain>
2014-08-06 10:19:44 -0700

    http post ftl DEBUG

--- main.tcl
+++ main.tcl
@@ -137,7 +137,7 @@ proc readreq {chan addr} {
 				set env(SCRIPT_FILENAME) "$filepfx($chan)${url}"
 				if {[dict exists headers($chan) cookie]} {set env(HTTP_COOKIE) [dict get headers($chan) cookie]}
 
-				set fromc [open "|$prog $filepfx($chan)${url}" rw]
+				set fromc [open "|$prog $filepfx($chan)${url}" r+]
 				if {[info exists postdata($chan)]} {puts $fromc $postdata($chan)}
 				puts -nonewline $chan "HTTP/1.1 200 Attempting to send results of script\r\n"
 				sendfromchan $chan $fromc
