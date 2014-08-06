89b93ce3c6e696b8aa8cc266fdc841c41447c4d4
Jack Johnson <jack@localhost.localdomain>
2014-08-06 10:19:03 -0700

    http post ftl DEBUG

--- main.tcl
+++ main.tcl
@@ -1,4 +1,4 @@
-#!/usr/bin/env tcl
+#!/usr/bin/env tclsh8.6
 
 lappend auto_path "[pwd]/lib"
 package require fileutil::magic::mimetype
@@ -137,7 +137,7 @@ proc readreq {chan addr} {
 				set env(SCRIPT_FILENAME) "$filepfx($chan)${url}"
 				if {[dict exists headers($chan) cookie]} {set env(HTTP_COOKIE) [dict get headers($chan) cookie]}
 
-				set fromc [open "|$prog $filepfx($chan)${url}"]
+				set fromc [open "|$prog $filepfx($chan)${url}" rw]
 				if {[info exists postdata($chan)]} {puts $fromc $postdata($chan)}
 				puts -nonewline $chan "HTTP/1.1 200 Attempting to send results of script\r\n"
 				sendfromchan $chan $fromc
